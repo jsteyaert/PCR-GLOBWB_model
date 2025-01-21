fe978f31fd08c0741667f110bed48bdb34c8860b
Jennie C. Steyaert <steya001@login01.cluster>
2025-01-21 13:29:35 +0100

    updated ini files and create job

---
+++ job/30_lc.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#SBATCH --output ./log/30sec_lc.out
+#SBATCH --error ./log/30sec_lc.err
+#SBATCH --time 96:00:00
+#SBATCH -n 32
+#SBATCH -J 30sec_lc
+
+
+
+
+# the %10 submits them 10 at a time :)
+echo "SBATCH job"
+echo "Started $(date '+%d/%m/%Y %H:%M:%S')"
+echo "Working directory $(pwd)"
+
+module load 2021
+module load Miniconda3/4.9.2
+
+#export PCRASTER_NR_WORKER_THREADS=32
+
+
+conda run -n pcr_model --no-capture-output python /eejit/home/steya001/climate_pcrglobwb/PCR-GLOBWB_model/model/deterministic_runner.py /eejit/home/steya001/climate_pcrglobwb/PCR-GLOBWB_model/config/30sec_jen/30_second_landcover.ini
+
+echo "Finished $(date '+%d/%m/%Y %H:%M:%S')"
+
+
+# H1
+# potentially to have one job per GCM with things to edit and comment out
+#START_DATE = 1990-01-01
+#END_DATE = 2000-01-01
---
+++ job/5min_lc.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#SBATCH --output ./log/5min_lc.out
+#SBATCH --error ./log/5min_lc.err
+#SBATCH --time 96:00:00
+#SBATCH -n 32
+#SBATCH -J 5min_lc
+
+
+
+
+# the %10 submits them 10 at a time :)
+echo "SBATCH job"
+echo "Started $(date '+%d/%m/%Y %H:%M:%S')"
+echo "Working directory $(pwd)"
+
+module load 2021
+module load Miniconda3/4.9.2
+
+#export PCRASTER_NR_WORKER_THREADS=32
+
+
+conda run -n pcr_model --no-capture-output python /eejit/home/steya001/climate_pcrglobwb/PCR-GLOBWB_model/model/deterministic_runner.py /eejit/home/steya001/climate_pcrglobwb/PCR-GLOBWB_model/config/30sec_jen/5min_landcover_30sec_model.ini
+
+echo "Finished $(date '+%d/%m/%Y %H:%M:%S')"
+
+
+# H1
+# potentially to have one job per GCM with things to edit and comment out
+#START_DATE = 1990-01-01
+#END_DATE = 2000-01-01
